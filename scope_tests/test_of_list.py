70c69530601bc2c401008eecb03ee37b70bd5591
Moshe Goldstein <moshegoldstein344@gmail.com>
2025-12-09 11:21:42 +0200

    Add files via upload

---
+++ scope_tests/test_of_list.py
@@ -0,0 +1,20 @@
+# number = [1,2,3,4,5,6,7,8,9]
+# color = ["red","blue","green","yellow","pink","purple"]
+# number [0] = 100
+# color[0] = "orange"
+# del color[1]
+# del number[8]
+# print(number)
+# print(color)
+# number_color = number + color
+# print(number_color)
+# print(len(number_color))
+# print(list("hello"))
+# namas = ["nsns","sdsds","dffdfd"]
+# print(namas[-1][-1])
+# int(2.7
+text = "good"
+print(text.find("a"))
+a = [1,2,3,7,8,9]
+print(a.index(8))
+
