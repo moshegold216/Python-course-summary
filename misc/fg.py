54f07e19ea0011d161d86a1814b71be2090f5e35
Moshe Goldstein <moshegoldstein344@gmail.com>
2025-12-09 11:30:04 +0200

    Add files via upload

---
+++ misc/fg.py
@@ -0,0 +1,11 @@
+# print("hello,world".count("l"))
+# example = {"a": 1, "b": 2, "c": 3}
+# print(example.items())
+# num = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# num3 = num +num2
+# print(num3)
+type(1.0)
+print("hello world"[3:])
+
+
