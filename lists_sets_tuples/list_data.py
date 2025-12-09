3cf1140b554460f15825a6a0bc93317cd568d478
Moshe Goldstein <moshegoldstein344@gmail.com>
2025-12-09 11:15:19 +0200

    Add files via upload

---
+++ lists_sets_tuples/list_data.py
@@ -0,0 +1,41 @@
+my_list = ["modhr",13,True]
+print(my_list) # to print the list
+# קודל כל מה זה ליסט ? ליסט זה משתנה המכיל רשימה של ערכים
+print(type(my_list)) # this is code to know what have in the list!
+# for example:
+# my_list  = [1, 2, 3]
+# print(type(my_list))
+########################################################################################################################################
+print(len(my_list)) # to know how much len we have !
+#  in list we can to mix all the elements together in on list
+# like that
+# my_list3= ["moshe",45,True]
+# print(my_list3)
+# print(my_list[2])
+# print(my_list[0])
+# to use in element in list. we do [in that we type the number]
+# print(my_list[0])
+# print(my_list[1:4]) # the element bein 1  to 4 = 2 and 3 :
+my_list.append("modhsdr") # to add  element to the  list
+girl = ["maya",34, False] #list 1
+girls = ["girl",45,True] #list 2
+onlist = girls +girl # to make 2 list to 1 = list1+list2
+print(onlist) #print the list of the 2
+onlist[3] = "yarom" # to change the element for object
+# print(onlist)
+onlist.insert(1,"66") # to change from the place of the object!
+#del onlist[1] # for delite the element from his place !
+#
+price = [3444.5555,5656566,565475,5775]
+price_max = max(price) # to know which price is the big on
+print(price_max)  # print that!
+
+
+
+
+
+
+
+
+#in this web tou can find all the Python List/Array Methods :
+# https://www.w3schools.com/python/python_ref_list.asp ### do not forget to remove that sign #
